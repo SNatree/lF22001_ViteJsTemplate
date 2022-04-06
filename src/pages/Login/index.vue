8aab41ca583eb95ee7914d9643fdb2a3f1db7765
Natree Sangrung <52654845+SNatree@users.noreply.github.com>
2022-04-06 11:48:37 +0700

    Update index.vue

--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -11,31 +11,6 @@ import router from './../../routes'
 import { useRouter, useRoute } from 'vue-router'
 export default defineComponent({
     setup() {
-        ;(async () => {
-            // router.addRoute({
-            //     path: '/',
-            //     name: 'home',
-            //     component: () => import('./../Home/index.vue'),
-            //     meta: {
-            //         layout: 'dashboard',
-            //         requiresAuth: false,
-            //     },
-            // })
-            // // router.options.routes.push({
-            // //     path: '/',
-            // //     name: 'home',
-            // //     component: () => import('./../Home/index.vue'),
-            // //     meta: {
-            // //         layout: 'dashboard',
-            // //         requiresAuth: false,
-            // //     },
-            // // })
-            // await router.isReady()
-            // // router.push({ name: 'home' })
-            // console.log('routers :>> ', router.getRoutes())
-            // console.log('router.options.routes :>> ', router.options.routes)
-        })()
-
         return {}
     },
 })
